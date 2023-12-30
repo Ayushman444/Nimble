7cc17ef241bac90549c996830de83f0bcfaf087e
Ayushman Tiwari <ayushman5022@gmail.com>
2023-12-31 03:56:26 +0530

    DB connection code

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,1 +1,16 @@
-//hello
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB connected successfully"))
+    .catch((error) => {
+      console.log("DB connnection failed");
+      console.error(error);
+      process.exit(1);
+    });
+};
