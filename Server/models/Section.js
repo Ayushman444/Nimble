81811176d0ee9d066ae1562df6d67de32db7ea2e
Ayushman Tiwari <ayushman5022@gmail.com>
2023-12-31 05:55:28 +0530

    Section and SubSection Schema code

---
+++ Server/models/Section.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const sectionSchema = new mongoose.Schema({
+  sectionName: {
+    type: String,
+  },
+  subSection: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "SubSection",
+  },
+  description: {
+    type: String,
+  },
+  videoUrl: {
+    type: String,
+  },
+});
+
+//model name and schema name
+module.exports = mongoose.model("Section", sectionSchema);
---
+++ Server/models/SubSection.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const subSectionSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  timeDuration: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  videoUrl: {
+    type: String,
+  },
+});
+
+//model name and schema name
+module.exports = mongoose.model("SubSection", subSectionSchema);
